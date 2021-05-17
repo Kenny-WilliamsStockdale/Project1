0a2351c2843484daa838893b7c806d0de088a9e7
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-05-17 14:34:01 +1200

    Another Form code and colours

--- Program.cs
+++ Program.cs
@@ -26,35 +26,78 @@ namespace Project1
             //Console.WriteLine("The sum is = " + c);  // printing out the total sum 
             //Console.ReadKey();
 
-            int EID, SALARY;
-            string ENAME, ADDRESS, JOB;     // declaring variables 
+            //int EID, SALARY;
+            //string ENAME, ADDRESS, JOB;     // declaring variables 
 
-            Console.WriteLine("Enter Emplyee Information"); // making a basic form. Taking input from the user and displaying as a list. 
+            //Console.WriteLine("Enter Emplyee Information"); // making a basic form. Taking input from the user and displaying as a list. 
 
-            Console.WriteLine("Enter the Employee ID");
-            EID = int.Parse(Console.ReadLine());        // Parsing number to string
+            //Console.WriteLine("Enter the Employee ID");
+            //EID = int.Parse(Console.ReadLine());        // Parsing number to string
 
-            Console.WriteLine("Enter the Employee Name");
-            ENAME = Console.ReadLine();                 // ReadLine requires data input or feedback on the console.
+            //Console.WriteLine("Enter the Employee Name");
+            //ENAME = Console.ReadLine();                 // ReadLine requires data input or feedback on the console.
 
-            Console.WriteLine("Enter the Employee Salary");
-            SALARY = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Employee Salary");
+            //SALARY = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the Employee Address");
-            ADDRESS = Console.ReadLine();
+            //Console.WriteLine("Enter the Employee Address");
+            //ADDRESS = Console.ReadLine();
 
-            Console.WriteLine("Enter the Employee Job Title");
-            JOB = Console.ReadLine();
+            //Console.WriteLine("Enter the Employee Job Title");
+            //JOB = Console.ReadLine();
 
-            Console.WriteLine("\n\n\nThe Employee Details: ");  // Not sure what the \n thing does. Is meant to be new line but don't see it working
+            //Console.WriteLine("\n\n\nThe Employee Details: ");  // Not sure what the \n thing does. Is meant to be new line but don't see it working
 
-            Console.WriteLine("Employee ID: " + EID);       // Printing out the list here by inserting users input 
-            Console.WriteLine("Employee Name: " + ENAME);
-            Console.WriteLine("Employee Salary: " + SALARY);
-            Console.WriteLine("Employee Address: " + ADDRESS);
-            Console.WriteLine("Employee Job: " + JOB);
+            //Console.WriteLine("Employee ID: " + EID);       // Printing out the list here by inserting users input 
+            //Console.WriteLine("Employee Name: " + ENAME);
+            //Console.WriteLine("Employee Salary: " + SALARY);
+            //Console.WriteLine("Employee Address: " + ADDRESS);
+            //Console.WriteLine("Employee Job: " + JOB);
+            //Console.ReadKey();
+
+            //int SNO, SDV503, COM502, CSA501, DES501;
+            //string SNAME;                                   // Created another input form 
+
+            //Console.WriteLine("Enter the student name");
+            //SNAME = Console.ReadLine();
+
+            //Console.WriteLine("Enter student number");
+            //SNO = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter student's Mark for SDV503");
+            //SDV503 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter student's Mark for COM502");
+            //COM502 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter student's Mark for CSA501");
+            //CSA501 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter student's Mark for DES501");
+            //DES501 = int.Parse(Console.ReadLine());
+
+            //int Average = (SDV503 + COM502 + CSA501 + DES501) / 4;      // made a number variable calculating the average of all student marks
+
+            //Console.WriteLine("\n\n\nThe Employee Details: ");  
+
+            //Console.WriteLine("Student Number: " + SNO);        
+            //Console.WriteLine("Student Name: " + SNAME);
+            //Console.WriteLine("SDV503 Mark: " + SDV503);
+            //Console.WriteLine("COM502 Mark: " + COM502);
+            //Console.WriteLine("CSA501 Mark: " + CSA501);
+            //Console.WriteLine("DES501 Mark: " + DES501);
+            //Console.WriteLine("SDV503 Mark: " + SDV503);
+            //Console.WriteLine("Student's Average Mark: " + Average);
+            //Console.ReadKey();
+
+            Console.BackgroundColor = ConsoleColor.Blue;    // changing the background and foreground colours in the console.
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "Welcome to C Sharp";           // Changes title on the console 
+
+            Console.WriteLine("Background color is blue");
+            Console.WriteLine("Foreground color is white");
+            Console.WriteLine("Welcome to C Sharp");
             Console.ReadKey();
-
         }
     }
 }
